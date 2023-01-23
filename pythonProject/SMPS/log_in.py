05f99bc3c19856e57c6f8032bc1c93907f620db7
harshil0521 <harshil.solanki@samudra.co.in>
2023-01-23 16:05:22 +0530

    Login page update

--- pythonProject/SMPS/log_in.py
+++ pythonProject/SMPS/log_in.py
@@ -2,28 +2,46 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 
-def log_in(driver):
 
+def log_in(driver):
     """Number & Password"""
     phone_number = driver.find_element(By.NAME, "phone_number")
     phone_number.clear()
-    phone_number.send_keys("9925044205")
     password = driver.find_element(By.NAME, "password")
     password.clear()
-    password.send_keys("admin123456")
+
+    """Local admin"""
+    # phone_number.send_keys("7777914868")
+    # password.send_keys("admin123456")
+
+    """Local BMC Collection"""
+    # phone_number.send_keys("9925044205")
+    # password.send_keys("admin123456")
+
+    """Live admin"""
+    # phone_number.send_keys("8141000000")
+    # password.send_keys("123")
+
+    """Live BMC Collection"""
+    phone_number.send_keys("8160095599")
+    password.send_keys("1234")
+
     password.send_keys(Keys.RETURN)
 
     """Dock & Shift selection"""
     dock_dropdown = driver.find_element(By.NAME, "dock")
     dock_dropdown.click()
+
     dock_selection = driver.find_element(By.ID, "mat-option-2")
     dock_selection.click()
+
     shift = driver.find_element(By.NAME, "shift")
     shift.click()
+
     selected_shift = driver.find_element(By.XPATH, "//*[contains(text(), 'Morning')]")
 #   selected_shift = driver.find_element(By.XPATH, "//*[contains(text(), 'Evening')]")
     selected_shift.click()
+
     submit = driver.find_element(By.XPATH, "//*[contains(text(), 'Submit')]")
     submit.click()
     time.sleep(0.5)
-
--- pythonProject/SMPS/main.py
+++ pythonProject/SMPS/main.py
@@ -4,19 +4,16 @@ import time
 from truck_arrival import truck_arrival
 from dock_collection import dock_collection
 
-
 driver = webdriver.Chrome()
-driver.get('http://localhost:4200/#/login')
 driver.implicitly_wait(10)
 driver.maximize_window()
 
+# driver.get('http://localhost:4200/#/login')
+driver.get('http://proc.smartmps.in/#/login')
 
 log_in(driver)
-
-
 # truck_arrival(driver)
-#dock_collection(driver)
-
+# dock_collection(driver)
 
 time.sleep(5)
 driver.quit()
